93464fa819a43e279df3d0165de03248d9ea099b
Dogue <dogue@oddhelix.com>
2024-08-25 21:49:20 -0400

    improve errors and make declared variables immutable

--- src/ht.nim
+++ src/ht.nim
@@ -1,15 +1,14 @@
 import macros
 
 macro `<-`*(a: untyped, b: typed) =
-    a.expectKind nnkTupleConstr
-    a.expectLen(2, 2)
-    let collections: set[NimTypeKind] = {ntyOpenArray, ntyArray, ntySequence}
-    let ok = b.getType().typeKind() in collections
-    if not ok:
-        error("expected array, openArray, or sequence", b)
+    if not (a.kind() == nnkTupleConstr and a.len() == 2):
+        error("left hand side must be a tuple constructor of length 2", a)
+
+    if not (b.getType().typeKind() in {ntyOpenArray, ntyArray, ntySequence}):
+        error("right hand side must be an openArray, array, or sequence", b)
 
     let x = a[0]
     let y = a[1]
 
     quote do:
-        var (`x`, `y`) = (`b`[0], `b`[1..^1])
+        let (`x`, `y`) = (`b`[0], `b`[1..^1])
